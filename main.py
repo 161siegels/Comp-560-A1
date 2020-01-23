709b4004dbc026a76f078fb8844f24b4d95819e4
James Bury <jamesbury77@gmail.com>
2020-01-23 15:31:04 -0500

    Improved spacing on main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from Main.startup import checkArguments
 from Main.fileReader import readFromFile
 
+
 def main():
     file_name = checkArguments()
     inputs = readFromFile(file_name)
     print(inputs)
+
 
 if __name__ == '__main__':
     main()
