ca824c76bae09d0cc2269acdcd669dd2d2ecb309
James Bury <jamesbury77@gmail.com>
2020-01-27 15:45:05 -0500

    Fixed checking for file in argument, now exits on error

--- Main/startup.py
+++ Main/startup.py
@@ -6,12 +6,12 @@ def checkArguments() -> str:
     if len(sys.argv) != 2:
         print("Usage: python main.py [TEXT FILENAME]")
         print("For example: \"python main.py usa.txt\"")
-        return ""
+        exit(1)
     else:
-        if path.exists("Files/usa.txt"):
+        if path.exists("Files/" + sys.argv[1]):
             print("Found file")
             return "Files/" + sys.argv[1]
         else:
             print("File not found, please make sure it was entered correctly.")
-            return ""
+            exit(1)
 
